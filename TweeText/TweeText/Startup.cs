5aba7c39cf027bbccde31ae1f0eff1ed7ef9f56a
Perconey <perconey@gmail.com>
2018-06-09 15:33:02 +0200

    Initial

---
+++ TweeText/TweeText/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TweeText.Startup))]
+namespace TweeText
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
